0fc67fc8cc84b86be9043ccd360cc4eca40562e0
cc22ji <chouhan.chetan222@gmail.com>
2023-12-17 17:12:09 +0530

    first portfolio html projectt

---
+++ assets/js/jsFile.js
@@ -0,0 +1,24 @@
+
+
+
+function flashcardGit(){ //https://github.com/cc22ji/FlashCard-Frontend
+    var url = 'https://github.com/cc22ji/FlashCard-Frontend';
+    window.open(url, '_blank');
+}
+
+function flashcardLive(){   //https://wondrous-shortbread-3bf122.netlify.app/
+    var url = 'https://wondrous-shortbread-3bf122.netlify.app/';
+    window.open(url, '_blank');
+}
+
+function YoutubeGit(){ //frontend-- https://github.com/cc22ji/YouTubeSubscriber-Frontend
+    var url = 'https://github.com/cc22ji/YoutubeSubscriber-Backend';     
+    window.open(url, '_blank');  // //backend -- https://github.com/cc22ji/YoutubeSubscriber-Backend
+
+}
+
+function youtubeLive(){     //backend -- https://youtubesubscriber-backend-rhdu.onrender.com/
+    var url = 'https://youtubesubscriber-frontend.onrender.com/';
+    window.open(url, '_blank');  // // frontend -- https://youtubesubscriber-frontend.onrender.com/
+}
+
